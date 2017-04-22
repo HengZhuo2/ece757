7143d459041b81ded723307cbf7cfc348799b498
zohan180 <hzhuo2@wisc.edu>
2017-04-21 19:56:17 -0500

    Create Vmutex.cc

---
+++ gem5/Vmutex.cc
@@ -0,0 +1,31 @@
+#include "Vmutex.hh"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+Vmutex::Vmutex(const Params *p) : 
+	SimObject(p)
+	// latency(params->time_to_wait)
+	// size(params ->size)
+{
+	std :: cout << "hello world! from vmutex object" << std::endl;
+}
+
+void
+Vmutex::check(int addr)
+{
+	std :: cout << "hello world! from vmutex object" << std::endl;
+}
+
+void
+Vmutex::unlock(int addr)
+{
+
+}
+
+
+Vmutex*
+vmutex_PParams::create()
+{
+	return new Vmutex(this);
+}
