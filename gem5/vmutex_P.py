66e29a44e60389c446763bd7a2270777eda84f54
zohan180 <hzhuo2@wisc.edu>
2017-04-21 19:59:23 -0500

    Create vmutex_P.py

---
+++ gem5/vmutex_P.py
@@ -0,0 +1,10 @@
+// gem5/mem/ruby/structures
+
+from m5.params import *
+from m5.proxy import *
+from m5.SimObject import SimObject
+
+class vmutex_P(SimObject):
+	type = 'vmutex_P'
+	cxx_class = "Vmutex"
+	cxx_header = "mem/ruby/structures/Vmutex.hh"
